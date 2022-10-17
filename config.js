94869adf25ebea97eed6590c002df6d16133f31b
animek12 <111511065+animek12@users.noreply.github.com>
2022-10-18 00:10:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -38,7 +38,7 @@ global.mess = {
 }
 global.limitawal = {
     premium: "Infinity",
-    free: 25
+    free: 1000
 }
 global.thumb = fs.readFileSync('./image/zens.jpg')
 global.vaze = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
